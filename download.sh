1f56df2887a761a0a08bd5c2619fb937ccf5c736
molin2050 <531486448@qq.com>
2018-07-04 17:08:25 +0800

    first commit

---
+++ download.sh
@@ -0,0 +1,4 @@
+git init
+git remote add origin git@github.com:linux-sunxi/u-boot-sunxi.git
+git fetch origin mirror/master
+git pull origin mirror/master
---
+++ update.sh
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:molin2050/uboot_h3_old.git
+git push -u origin master
